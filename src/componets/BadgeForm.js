5f59c6f33f6f4df90ca85e3fe608de83a94e270c
Valentina <valen.caldera17@gmail.com>
2019-12-04 12:01:47 -0500

    Modified BadgeForm and BadgeNew files

--- src/componets/BadgeForm.js
+++ src/componets/BadgeForm.js
@@ -67,6 +67,10 @@ class BadgeForm extends React.Component{
                     </div>
 
                     <button onClick={this.onSubmit} className="btn btn-primary">Save</button>
+
+                    {this.props.error && (
+                    <p className="text-danger">{this.props.error.message}</p>
+                    )}
                 </form>
             </div>
         )
